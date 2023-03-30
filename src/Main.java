308f70869d245ee63c3dc600b6cff936c84d51b7
Gulzhanar23 <berikgulzhanar@gmail.com>
2023-03-29 21:24:04 -0400

    the color added

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,15 @@ public class Main {
 BasicsStructure audi=new BasicsStructure();
         audi.color="black";
         audi.model="A3";
-        audi.power="5000cc";    }
+        audi.power="5000cc";
+
+
+        color col=new color();
+        col.colorLights="true";
+        col.colorWindows="false";
+        col.colorTyre="true";
+
+
+
+    }
 }
